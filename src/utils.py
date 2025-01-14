d9d2d00830aa2bf351a40e0e52de44ff41758dcf
Vishal More <130221313+Vishal2053@users.noreply.github.com>
2025-01-14 14:02:04 +0530

    data trainer and hyperparameter added

--- src/utils.py
+++ src/utils.py
@@ -22,36 +22,35 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+        for model_name, model in models.items():
+            para = param[model_name]
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            # Skip GridSearch for XGBRegressor and CatBoost
+            if model_name in ["XGBRegressor", "CatBoosting Regressor"]:
+                model.fit(X_train, y_train)
+                best_model = model
+            else:
+                gs = GridSearchCV(model, para, cv=3, n_jobs=-1, verbose=2)
+                gs.fit(X_train, y_train)
+                best_model = gs.best_estimator_
 
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
-
-            y_train_pred = model.predict(X_train)
-
-            y_test_pred = model.predict(X_test)
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise e
+
     
 def load_object(file_path):
     try:
